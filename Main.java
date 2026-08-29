80ec38dc5a1ed7019947759f271493c032e6bd43
e31262596-code <e31262596@student.polije.ac.id>
2026-08-30 01:45:23 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,23 +1,20 @@
-package konstanta;
-
-import javax.swing.JOptionPane;
+package literal;
 
 public class Main {
     public static void main(String[] args) {
-        // Deklarasi konstanta
-        final double PI = 3.14159;
+        int bil = 10;
+        char karakter = '\u0041';
+        boolean benar = true;
+        boolean salah = false;
+        float bilangan = 1.234f;
+        double angka = 3.767d;
+        String nama = "Gibran";
 
-        // Deklarasi variabel jari2 dan luas
-        int jari2;
-        double luas;
-
-        // Memberi nilai awal ke variabel jari2
-        jari2 = 7;
-
-        // Menghitung luas lingkaran
-        luas = PI * jari2 * jari2;
-
-        System.out.println("Jari-jari lingkaran = " + jari2);
-        System.out.println("\nLuas lingkaran = " + luas);
+        System.out.println("int bil: " + bil);
+        System.out.println("char karakter: " + karakter);
+        System.out.println("boolean benar : " + benar);
+        System.out.println("boolean salah : " + salah);
+        System.out.println("float bilangan: " + bilangan);
+        System.out.println("String nama: " + nama);
     }
 }
