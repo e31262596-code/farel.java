c70637201a9f32a4d2cb238683c4af739a0b0f0b
e31262596-code <e31262596@student.polije.ac.id>
2026-08-30 01:14:38 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package Tugasjava_a;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 24;
+        int UMUR = 25;
+
+        System.out.println(umur); // prints 24
+        System.out.println(UMUR); // prints 25
+    }
+}
