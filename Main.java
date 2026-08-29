2d1eb7d91f7964a17c92f5ff5c7d0a9425543ffa
e31262596-code <e31262596@student.polije.ac.id>
2026-08-30 01:58:10 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,20 +1,8 @@
-package literal;
+package tugas_d;
 
 public class Main {
     public static void main(String[] args) {
-        int bil = 10;
-        char karakter = '\u0041';
-        boolean benar = true;
-        boolean salah = false;
-        float bilangan = 1.234f;
-        double angka = 3.767d;
-        String nama = "Gibran";
-
-        System.out.println("int bil: " + bil);
-        System.out.println("char karakter: " + karakter);
-        System.out.println("boolean benar : " + benar);
-        System.out.println("boolean salah : " + salah);
-        System.out.println("float bilangan: " + bilangan);
-        System.out.println("String nama: " + nama);
+        int chr = 'A';
+        System.out.println("The ASCII value of A is :" + chr);
     }
 }
