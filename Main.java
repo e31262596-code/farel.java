ef4bf6a174323c89b7b59d79d33e67f4af95f161
e31262596-code <e31262596@student.polije.ac.id>
2026-08-30 02:03:06 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,8 +1,21 @@
-package tugas_d;
-
 public class Main {
+
     public static void main(String[] args) {
-        int chr = 'A';
-        System.out.println("The ASCII value of A is :" + chr);
+
+        // Deklarasi variabel
+        int a, b, temp;
+        a = 15;
+        b = 27;
+
+        // Menampilkan nilai sebelum ditukar
+        System.out.println("Before swapping : a, b = " + a + ", " + b);
+
+        // Proses penukaran nilai menggunakan variabel sementara (temp)
+        temp = a;
+        a = b;
+        b = temp;
+
+        // Menampilkan nilai setelah ditukar
+        System.out.println("After swapping : a, b = " + a + ", " + b);
     }
 }
